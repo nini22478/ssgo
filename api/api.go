b5f72711fd707d618c2542763eadfd7b099ee179
mxnmac <511541512@qq.com>
2023-11-22 16:10:08 +0800

    新表

--- api/api.go
+++ api/api.go
@@ -140,8 +140,8 @@ func (c *APIClient) Init() error {
 }
 func (c *APIClient) GetUsers() (retc *UserRets, err error) {
 
-	// path := "/api/SsGetUsers"
-	path := "http://47.92.220.167/api/SsGetUsersFirm'"
+	path := "/api/SsGetUsersFirm"
+	// path := "http://47.92.220.167/api/SsGetUsersFirm'"
 	retc = &UserRets{}
 	// c.client.SetQueryParam("n", strconv.Itoa(*c.NodeID))
 	client := resty.New()
