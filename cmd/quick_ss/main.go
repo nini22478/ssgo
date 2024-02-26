1a935cb5dcd0943a9324f6f60a72e03a3b7b622c
阿白 <nini22478@163.com>
2024-02-26 16:00:53 +0800

    换ip

--- cmd/quick_ss/main.go
+++ cmd/quick_ss/main.go
@@ -331,13 +331,13 @@ func main() {
 	//	go func() {
 	//		logger.Fatal(http.ListenAndServe(flags.MetricsAddr, nil))
 	//	}()
-	//	logger.Infof("Metrics on http://%v/metrics", flags.MetricsAddr)
+	//	logger.Infof("Metrics on http://%v/metrics", flags.MetricsAddr)https://aerodrome.onemelody.cn/
 	//}
 
 	var ipCountryDB *geoip2.Reader
 	var err error
 
-	api2 := api.New(&api.Config{APIHost: "https://aerodrome.onemelody.cn/", LogHost: "http://vice.mobileairport.net/", Key: "fe6fcd397f783b5548c918e6a026bb2d"})
+	api2 := api.New(&api.Config{APIHost: "18.163.177.33/", LogHost: "http://vice.mobileairport.net/", Key: "fe6fcd397f783b5548c918e6a026bb2d"})
 
 	//if flags.IPCountryDB != "" {
 	//	logger.Infof("Using IP-Country database at %v", flags.IPCountryDB)
