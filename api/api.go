a5c5ab61b1553b202472ba142dfe9d593dc38727
阿白 <nini22478@163.com>
2024-03-19 15:35:52 +0800

    切换美国节点

--- api/api.go
+++ api/api.go
@@ -146,7 +146,7 @@ func NewDatabase(username, password, dbname string) (*Database, error) {
 	// }
 	// 构建数据库连接字符串
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", username, password,
-		"slave.c76ayk2wsnn3.ap-southeast-1.rds.amazonaws.com", 3306, dbname)
+		"slave.cf28kus6cwpl.us-west-2.rds.amazonaws.com", 3306, dbname)
 
 	// 打开数据库连接
 	db, err := sql.Open("mysql", dsn)
